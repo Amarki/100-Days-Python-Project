07dfd8c295c2cfd09bc954557361d3ec8f13592f
Amarki <amarki1337@gmail.com>
2024-11-13 08:58:49 -0800

    Add files via upload

---
+++ Day 1 - Band Name Generator.py
@@ -0,0 +1,7 @@
+# Day 2 - Band Name Generator
+# Purpose: To ask user for city name and pet name to combine them into a generated band name.
+print("Let's create a band name together!")
+city = input("What city did you grow up in?\n")
+petName = input("What was the name of a pet you had?\n")
+
+print("Your band name is: " + city + " " + petName + "!")
